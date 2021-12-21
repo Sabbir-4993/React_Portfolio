13355aec6b807807157a00a3b8ebb731fabc8122
Sabbir Ahmed <sabbir15-4993@diu.edu.bd>
2021-12-21 18:08:01 +0600

    final

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,9 +5,8 @@ import ServiceList from "../Component/Home/ServiceList";
 // import Testimonial from "../Component/Home/Testimonial";
 import Experience from "../Component/Home/Experience";
 import Clients from "../Component/Home/Clients";
-import PriceTable from "../Component/Home/PriceTable";
+// import PriceTable from "../Component/Home/PriceTable";
 import HobbyLanguage from "../Component/Home/HobbyLanguage";
-import Testimonial from "../Component/Home/Testimonial";
 
 class Home extends React.Component{
     render() {
